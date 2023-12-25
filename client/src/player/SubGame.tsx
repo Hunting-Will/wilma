56c8c218070e13a37adef64d259c74cd5bd7307c
Alex Jilkin <jilkinalex@gmail.com>
2023-12-25 22:14:59 +0200

    Work on SubGame view

--- client/src/player/SubGame.tsx
+++ client/src/player/SubGame.tsx
@@ -1,13 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 import Box from "@mui/material/Box";
 import { fetchGame } from '../serverClient';
-import { Game, Player } from '../../../types';
+import type { Game, Player, GameAction } from '../../../types/';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 100
+}));
+
 
 export function SubGame() {
     const [player, setPlayer] = useState<Player>()
     const [game, setGame] = useState<Game>()
+
+    const [actions, setAction] = useState(['Seed', 'Harvest'])
 
     const { id } = useParams();
     const playerId = localStorage.getItem('playerId')
@@ -30,14 +44,30 @@ export function SubGame() {
     }, [])
 
     return (
-
         <Box
-            flexDirection="column"
             display="flex"
-            justifyContent="space-between"
+            justifyContent="center"
             alignItems="center"
-        >
-            Game {id}, Player {JSON.stringify(player)}
+            height="99vh"
+            padding="0 15px 0 15px">
+            <Box
+                flexDirection="column"
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                height="90vh"
+            >
+                <Box>
+                    Game {id}, Player {JSON.stringify(player)}
+                </Box>
+                <Grid container spacing={2}>
+                    {actions.map((action) =>
+                        <Grid item xs={4}>
+                            <Item>{action}</Item>
+                        </Grid>
+                    )}
+                </Grid>
+            </Box>
         </Box>
     );
 }
