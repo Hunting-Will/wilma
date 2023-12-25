7ab701fab9d8f2b6116820f66ea0f322c6cbd4ec
Alex Jilkin <jilkinalex@gmail.com>
2023-12-25 14:25:41 +0200

    ADd route

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,55 @@
 import Koa from 'koa'
-const app = new Koa();
+import Router from '@koa/router';
+// import bodypareser from 'koa-bodyparser';
+import redis from 'ioredis'
 
-console.log("Starting server on 3000")
-app.use(async ctx => {
-    ctx.body = 'Hello World';
+const app = new Koa();
+const router = new Router({ prefix: '/api' });
+
+function generateGameCode() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+router.get('/createGame/:lobbyName', async (ctx) => {
+  const lobbyName = ctx.params.lobbyName;
+
+  if (!lobbyName) {
+    ctx.status = 400;
+    ctx.body = { error: 'Lobby name is required' };
+    return;
+  }
+
+  const gameCode = generateGameCode();
+  const gameData = {
+    key: gameCode,
+    players: [], // Initially an empty array
+    data: {} // Empty object, to be filled with game-specific data
+  };
+
+  // Save game data in Redis
+  await redis.set(gameCode, JSON.stringify(gameData));
+
+  ctx.status = 200;
+  ctx.body = {
+    message: 'Game created successfully',
+    lobbyName: lobbyName,
+    gameCode: gameCode
+  };
 });
 
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
 app.on('error', (err, ctx) => {
-    console.error('server error', err, ctx)
+  console.error('server error', err, ctx)
 });
 
 app.listen(3000);
+console.log("Starting server on 3000")
