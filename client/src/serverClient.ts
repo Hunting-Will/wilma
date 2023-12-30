3db2a2adca0fa1293850e38c9259c68cc14bd9e6
Alex Jilkin <jilkinalex@gmail.com>
2023-12-30 10:35:17 +0200

    use env

--- client/src/serverClient.ts
+++ client/src/serverClient.ts
@@ -1,6 +1,8 @@
 import { GameController } from '../../game-logic/GameController';
 import { Game, GameAction } from '../../types/index'
-const baseUrl = 'http://localhost:3001';
+
+// const baseUrl = 'http://localhost:3001';
+const baseUrl = process.env.ENV === 'prod' ? 'https://wilma-server.onrender.com' : 'http://localhost:3001';
 
 export const joinGame = (id: string, nickname: string): Promise<{ playerId: string }> =>
     fetch(`${baseUrl}/api/joinGame/${id}/${nickname}`).then(res => res.json())
